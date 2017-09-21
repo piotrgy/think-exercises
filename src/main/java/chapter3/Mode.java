dccb600c426682bd6515625ea13437687b1d5436
piotrgy <piotr_gy@wp.pl>
2017-09-21 13:04:31 +0200

    Calculating mode by the help of a histogram array.

--- src/main/java/chapter3/Mode.java
+++ src/main/java/chapter3/Mode.java
@@ -30,8 +30,26 @@ public class Mode { //dominating value
         return result;
     }
 
+    static public int[] calculateModeByHistogram(int[] histogramArray) {
+        int mostFrequent = 0;
+        int highestFrequency;
+
+        for (int i = 0; i < histogramArray.length; i++) {
+            if (histogramArray[i] > histogramArray[mostFrequent]) {
+                mostFrequent = i;
+            }
+        }
+        highestFrequency = histogramArray[mostFrequent];
+        mostFrequent++; //because value is one greater than index in histogramArray
+
+        int[] result = new int[2];
+        result[0] = mostFrequent;
+        result[1] = highestFrequency;
+        return result;
+    }
+
     public static int[] histogram(int maxValue, int[] intsArray) {
-        int [] histogram = new int[maxValue];
+        int[] histogram = new int[maxValue];
         for (int i = 0; i < maxValue; i++) {
             histogram[i] = 0;
         }
