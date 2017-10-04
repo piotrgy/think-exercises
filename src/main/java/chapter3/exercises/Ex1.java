9b526a629bb215ebed0b38eea8c1163b5ac88aab
piotrgy <piotr_gy@wp.pl>
2017-10-04 14:36:47 +0200

    Class checks sorting array of objects.

---
+++ src/main/java/chapter3/exercises/Ex1.java
@@ -0,0 +1,33 @@
+package chapter3.exercises;
+
+import java.util.Arrays;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        int[] grades = {87, 28, 100, 78, 84, 98};
+        int[] studentIDs = {10002, 10003, 10004, 10005, 10006, 10007};
+        String[] studentsNames = {"Chris", "Robert", "John", "Alice", "Kate", "Maria"};
+
+        int size = grades.length;
+        Student[] students = new Student[size];
+        for (int i = 0; i < size; i++) {
+            students[i] = new Student(grades[i], studentIDs[i], studentsNames[i]);
+        }
+
+        Arrays.sort(students);
+        System.out.println("Students sorted by grade.");
+        displayStudents(students);
+
+        Arrays.sort(students, Student.StudentNameComparator);
+        System.out.println("Students sorted by name.");
+        displayStudents(students);
+    }
+
+    private static void displayStudents(Student[] students) {
+        for (int i = 0; i < students.length; i++) {
+            System.out.println((i + 1) + ". " + students[i]);
+        }
+    }
+}
