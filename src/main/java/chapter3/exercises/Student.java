696315f07069759087fe07519d874c9cb5ab9d7f
piotrgy <piotr_gy@wp.pl>
2017-10-04 14:34:22 +0200

    Class represents a student. Implements Comparable, contains static comparator method to compare student's names.

---
+++ src/main/java/chapter3/exercises/Student.java
@@ -0,0 +1,53 @@
+package chapter3.exercises;
+
+import java.util.Comparator;
+
+public class Student implements Comparable<Student> {
+    private int grade;
+    private int studentID;
+    private String name;
+
+    public Student(int grade, int studentID, String name) {
+        this.grade = grade;
+        this.studentID = studentID;
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getStudentName() {
+        return name;
+    }
+
+    public int compareTo(Student student) {
+        int comparedGrade = student.getGrade();
+
+        //ascending
+        //return this.grade - comparedGrade;
+
+        //descending
+        return comparedGrade - this.grade;
+    }
+
+    public static Comparator<Student> StudentNameComparator = new Comparator<Student>() {
+        public int compare(Student student1, Student student2) {
+            String student1Name = student1.getStudentName();
+            String student2Name = student2.getStudentName();
+
+            //ascending
+            //return student1Name.compareToIgnoreCase(student2Name);
+
+            //descending
+            return student2Name.compareToIgnoreCase(student1Name);
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "StudentID: " + studentID +
+                ", name: " + name +
+                ", grade: " + grade;
+    }
+}
